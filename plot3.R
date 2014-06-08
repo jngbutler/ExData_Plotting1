9c122f292029770d03a76e4beb360b90e3a6babd
jngbutler <jngbutler@users.noreply.github.com>
2014-06-08 08:16:55 -0400

    peer assignment submission

--- plot3.R
+++ plot3.R
@@ -44,5 +44,4 @@ lines(EPCsubset$datetime,EPCsubset$Sub_metering_2,col="red")
 lines(EPCsubset$datetime,EPCsubset$Sub_metering_3,col="blue")
 legend("topright",lty=1, col=c("black","red","blue"), 
        legend=c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"))
-dev.off()
-
+dev.off() 
